203b5c408f430a61705f83a5574ddfcb9c67d1e5
Alvin Dennis <alvindennis80@gmail.com>
2025-09-03 00:24:44 +0530

    fix: tsconfig , add middleware

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,10 +5,17 @@ import { appRouter } from "./routers/index";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
+import { timing } from "hono/timing";
+import { prettyJSON } from "hono/pretty-json";
+import { secureHeaders } from "hono/secure-headers";
+
 
 const app = new Hono();
 
 app.use(logger());
+app.use(timing());
+app.use(prettyJSON());
+app.use(secureHeaders());
 app.use(
 	"/*",
 	cors({
